b1f6de55c2bd84eaa47c546b13b3122b8526aa88
Yuri Martins <seu-email-do-github@gmail.com>
2026-08-24 17:06:29 -0300

    fix: Usa @neondatabase/serverless para conexao WebSockets na Vercel

--- src/api/db.ts
+++ src/api/db.ts
@@ -1,10 +1,12 @@
-import { Pool } from 'pg'
-import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool, neonConfig } from '@neondatabase/serverless'
+import { PrismaNeon } from '@prisma/adapter-neon'
 import { PrismaClient } from '@prisma/client'
+import ws from 'ws'
 
+neonConfig.webSocketConstructor = ws
 const connectionString = process.env.DATABASE_URL
 const pool = new Pool({ connectionString })
-const adapter = new PrismaPg(pool)
+const adapter = new PrismaNeon(pool)
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
