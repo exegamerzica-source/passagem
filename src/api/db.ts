1002212cfc4929f62fa8ed6aadc847edf728c153
Yuri Martins <seu-email-do-github@gmail.com>
2026-08-24 17:08:04 -0300

    fix: Usa fallback para a connection string para resolver problema de variavel de ambiente na Vercel

--- src/api/db.ts
+++ src/api/db.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client'
 import ws from 'ws'
 
 neonConfig.webSocketConstructor = ws
-const connectionString = process.env.DATABASE_URL
+const connectionString = process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_u6p0jDgsQBUl@ep-cold-dew-axt1axy2-pooler.c-4.us-east-2.aws.neon.tech/neondb?sslmode=require'
 const pool = new Pool({ connectionString })
 const adapter = new PrismaNeon(pool)
 
